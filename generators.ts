3bd58103413bdbe112ab5dbeb21f90b2af625ec1
jessyjones <amarsy.alya@gmail.com>
2025-08-11 21:17:28 +0200

    Update generators.ts

--- generators.ts
+++ generators.ts
@@ -1,16 +1,16 @@
-Blockly.circuitpython['dict_constructor'] = function(block) {
+Blockly.Python['dict_constructor'] = function(block) {
     const code = `dict()`;
     return [code, 0];
 };
 
-Blockly.circuitpython['dict_brackets'] = function(block) {
+Blockly.Python['dict_brackets'] = function(block) {
     const dict_name = Blockly.Python.nameDB_.getName(block.getFieldValue('dict_name'), Blockly.VARIABLE_CATEGORY_NAME);
     const parameters = Blockly.Python.valueToCode(block, 'parameters', 0);
     const code = `${dict_name}[${parameters}]`;
     return [code, 0];
 };
 
-Blockly.circuitpython['dict_brackets_equals'] = function(block) {
+Blockly.Python['dict_brackets_equals'] = function(block) {
     const dict_name = Blockly.Python.nameDB_.getName(block.getFieldValue('dict_name'), Blockly.VARIABLE_CATEGORY_NAME);
     const key = Blockly.Python.valueToCode(block, 'key', 0);
     const val = Blockly.Python.valueToCode(block, 'val', 0);
